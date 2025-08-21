9a595a88563b01e6dac7d3303fb39f631e91386e
Euunii <eunicetvr.love@gmail.com>
2025-08-20 23:50:46 -0300

    Finalización Tp4

--- temperatura.php
+++ temperatura.php
@@ -1,12 +1,18 @@
 <?php
 $gradosC = $_POST["temperatura"];
-$gradosF = 0;
-$gradosK = 0;
+$gradosF;
+$gradosK;
+
 function celsiusAF($gradosC){
     return ($gradosC * 9/5) + 32;
 
 };
 function celsiusAK ($gradosC){
-    return($gradosC);
+    return($gradosC + 273.15);
 };
+$gradosF = celsiusAF($gradosC);
+$gradosK = celsiusAK($gradosC);
+
+echo "La temperatura ingresa ". $gradosC. "°C es, " . $gradosF."°F convertidos en Fahrenheit y ". $gradosK. "°K convertidos en Kelvin";
+
 ?>
